dcdcf3a21f3c989d6b8ce91598b2d35d904ebd2d
tamimArt1 <mmmtamim@outlook.com>
2021-09-02 18:57:45 +0600

    patch error removed

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import express from "express";
 import { MongoClient, ObjectId } from "mongodb";
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-// app configs
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -13,53 +12,48 @@ app.use(
   })
 );
 
-// MongoDB
-const client = new MongoClient(process.env.CONNECTION_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+async function main() {
+  const client = new MongoClient(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
-// Make connection once and use it
-async function run() {
   try {
     await client.connect();
     app.listen(process.env.PORT || 9000);
-    const database = client.db("todo");
-    const todos = database.collection("todos");
+    const collection = client.db("todo").collection("todos");
 
     // Endpoints
-    app.get("/", async (req, res) => {
+    app.get("/", (req, res) => {
       res.send("todo crud api");
     });
 
     app.get("/todos", async (req, res) => {
-      const allTodos = await todos.find({}).toArray();
+      const allTodos = await collection.find({}).toArray();
       res.send(allTodos);
     });
 
     app.post("/todos", async (req, res) => {
       const newTodo = req.body;
-      await todos.insertOne(newTodo);
-      res.send("Insert done");
+      await collection.insertOne(newTodo);
+      res.send("Insert Done");
     });
 
     app.patch("/todo/:id", async (req, res) => {
       const id = ObjectId(req.params.id);
       const { text, completed } = req.body;
-      await todos.updateOne(
-        { _id: id },
-        { $set: { text: text, completed: completed } }
-      );
-      res.end();
+      await collection.updateOne({ _id: id }, { $set: { text, completed } });
+      res.send("Update Done");
     });
 
     app.delete("/todo/:id", async (req, res) => {
       const id = ObjectId(req.params.id);
-      await todos.deleteOne({ _id: id });
-      res.end();
+      await collection.deleteOne({ _id: id });
+      res.send("Delete Done");
     });
-  } finally {
+  } catch (err) {
+    console.error(err);
   }
 }
-run().catch(console.dir);
-client.close();
+
+main().catch(console.error);
