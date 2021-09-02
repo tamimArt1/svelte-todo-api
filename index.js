81fcf4e122fb7f6bf3c42dd62d9cd2174abc8d04
tamimArt1 <mmmtamim@outlook.com>
2021-09-02 13:37:18 +0600

    solved cors

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // MongoDB
 const client = new MongoClient(process.env.CONNECTION_URL, {
